934340eec630b7c27426f0768a1ea0403d89f912
Baggers <chris.bagley@gmail.com>
2016-10-18 22:41:41 +0200

    universal joint funcs

--- joints/universal.lisp
+++ joints/universal.lisp
@@ -6,9 +6,27 @@
                             (:include joint)))
 
 newtonconstraintcreateuniversal
-newtonuniversalcalculatestopalpha0
-newtonuniversalcalculatestopalpha1
-newtonuniversalgetjointangle1
-newtonuniversalgetjointforce
-newtonuniversalgetjointomega0
-newtonuniversalgetjointomega1
+
+;;------------------------------------------------------------
+
+(defun universal-joint-force (universal-joint)
+  (with-foreign-object (v3 :float 3)
+    (newtonuniversalgetjointforce (%joint-ptr universal-joint) v3)
+    (ptr->v3 v3)))
+
+(defun universal-joint-angle (universal-joint)
+  (newtonuniversalgetjointangle1 (%joint-ptr universal-joint)))
+
+(defun universal-joint-omega-0 (universal-joint)
+  (newtonuniversalgetjointomega0 (%joint-ptr universal-joint)))
+
+(defun universal-joint-omega-1 (universal-joint)
+  (newtonuniversalgetjointomega1 (%joint-ptr universal-joint)))
+
+;;------------------------------------------------------------
+
+;; (defun universal-joint-calc-stop-alpha-0 (universal-joint desc-ptr angle)
+;;   (newtonuniversalcalculatestopalpha0 (%joint-ptr universal-joint) desc-ptr angle))
+
+;; (defun universal-joint-calc-stop-alpha-1 (universal-joint desc-ptr angle)
+;;   (newtonuniversalcalculatestopalpha1 (%joint-ptr universal-joint) desc-ptr angle))
