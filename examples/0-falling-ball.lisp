40a54842fa157cd3f025aae415be540b6236998d
Baggers <chris.bagley@gmail.com>
2016-10-19 20:45:00 +0200

    examples folder

--- examples/0-falling-ball.lisp
+++ examples/0-falling-ball.lisp
@@ -10,11 +10,11 @@
   (multiple-value-bind (mass) (body-mass body)
     (setf (body-force body) (v! 0s0 (* -9.8 mass) 0s0 0s0))))
 
-(defun create-background-body (world)
+(defun create-floor (world)
   (with-geometry (geom (make-geometry-tree world points))
     (make-body world geom)))
 
-(defun create-freefall-ball (world)
+(defun create-ball (world)
   (with-geometry (geom (make-sphere-geometry world :radius 1s0))
     (let ((body (make-body world geom :linear-damping 0s0
                            :mass 1s0)))
@@ -22,12 +22,12 @@
       (setf (body-matrix4 body) (m4:translation (v! 0 40 -10)))
       body)))
 
-(defvar world (make-world))
-(defvar background-body (create-background-body world))
-(defvar freefall-ball (create-freefall-ball world))
+(defvar *world* (make-world))
+(defvar *floor* (create-floor *world*))
+(defvar *ball* (create-ball *world*))
 
 (defun newt ()
   (let ((step (/ 1s0 60)))
     (loop :for i :below 300 :do
-       (world-step world step)
-       (print (body-matrix4 freefall-ball)))))
+       (world-step *world* step)
+       (print (body-matrix4 *ball*)))))
