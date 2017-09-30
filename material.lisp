e0781b822225e93541c54be31030d9c5942c44c4
baggers <baggers@cave>
2017-09-30 04:30:27 +0200

    typed materials

--- material.lisp
+++ material.lisp
@@ -63,7 +63,9 @@
   (newtonmaterialgetcontactfaceattribute
    (%material-pair-ptr material-pair)))
 
-(defun material-pair-contact-force (material-pair body)
+(defn material-pair-contact-force ((material-pair material-pair)
+                                   (body body))
+    single-float
   (declare (optimize (speed 3) (safety 1) (debug 1)))
   (with-foreign-object (force :float)
     (newtonmaterialgetcontactforce
@@ -72,28 +74,36 @@
      force)
     (mem-aref force :float)))
 
-(defun material-pair-contact-max-normal-impact (material-pair)
+(defn material-pair-contact-max-normal-impact ((material-pair material-pair))
+    single-float
   (declare (optimize (speed 3) (safety 1) (debug 1)))
   (newtonmaterialgetcontactmaxnormalimpact
    (%material-pair-ptr material-pair)))
 
-(defun material-pair-contact-max-tangent-impact (material-pair index)
+(defn material-pair-contact-max-tangent-impact ((material-pair material-pair)
+                                                (index (signed-byte 32)))
+    single-float
   (declare (optimize (speed 3) (safety 1) (debug 1)))
   (newtonmaterialgetcontactmaxtangentimpact
    (%material-pair-ptr material-pair)
    index))
 
-(defun material-pair-contact-normal-speed (material-pair)
+(defn material-pair-contact-normal-speed ((material-pair material-pair))
+    single-float
   (declare (optimize (speed 3) (safety 1) (debug 1)))
   (newtonmaterialgetcontactnormalspeed
    (%material-pair-ptr material-pair)))
 
-(defun material-pair-contact-penetration (material-pair)
+(defn material-pair-contact-penetration ((material-pair material-pair))
+    single-float
   (declare (optimize (speed 3) (safety 1) (debug 1)))
   (newtonmaterialgetcontactpenetration
    (%material-pair-ptr material-pair)))
 
-(defun material-pair-contact-tangent-directions (material-pair body)
+(defn material-pair-contact-tangent-directions ((material-pair material-pair)
+                                                (body body))
+    (values (simple-array single-float (3))
+            (simple-array single-float (3)))
   (declare (optimize (speed 3) (safety 1) (debug 1)))
   (with-foreign-objects ((t0 :float 3)
                          (t1 :float 3))
@@ -102,83 +112,101 @@
      (%body-ptr body) t0 t1)
     (values (ptr->v3 t0) (ptr->v3 t1))))
 
-(defun material-pair-contact-tangent-speed (material-pair index)
+(defn material-pair-contact-tangent-speed ((material-pair material-pair)
+                                           (index (signed-byte 32)))
+    single-float
   (declare (optimize (speed 3) (safety 1) (debug 1)))
   (newtonmaterialgetcontacttangentspeed
    (%material-pair-ptr material-pair)
    index))
 
-(defun material-pair-set-callback-userdata (material-pair
-                                            material-a
-                                            material-b
-                                            ptr)
+(defn material-pair-set-callback-userdata ((material-pair material-pair)
+                                           (material-a (signed-byte 32))
+                                           (material-b (signed-byte 32))
+                                           (ptr foreign-pointer))
+    null
   (declare (optimize (speed 3) (safety 1) (debug 1)))
   (newtonmaterialsetcallbackuserdata
    (%material-pair-ptr material-pair)
    material-a
    material-b
    ptr)
-  (values))
+  nil)
 
-(defun material-pair-set-contact-elasticity (material-pair restitution)
+(defn material-pair-set-contact-elasticity ((material-pair material-pair)
+                                            (restitution single-float))
+    null
   (declare (optimize (speed 3) (safety 1) (debug 1)))
   (newtonmaterialsetcontactelasticity
    (%material-pair-ptr material-pair)
    restitution)
-  (values))
+  nil)
 
-(defun material-pair-set-contact-friction-coef (material-pair
-                                                static-friction-coefficient
-                                                kinetic-friction-coefficient
-                                                index)
+(defn material-pair-set-contact-friction-coef
+    ((material-pair material-pair)
+     (static-friction-coefficient single-float)
+     (kinetic-friction-coefficient single-float)
+     (index (signed-byte 32)))
+    null
   (declare (optimize (speed 3) (safety 1) (debug 1)))
   (newtonmaterialsetcontactfrictioncoef
    (%material-pair-ptr material-pair)
    static-friction-coefficient
    kinetic-friction-coefficient
    index)
-  (values))
+  nil)
 
-(defun material-pair-set-contact-friction-state (material-pair state index)
+;;;-------arse
+
+
+(defn material-pair-set-contact-friction-state ((material-pair material-pair)
+                                                (state (signed-byte 32))
+                                                (index (signed-byte 32)))
+    null
   (declare (optimize (speed 3) (safety 1) (debug 1)))
   ;; {TODO} what is the state in lisp? (its an int in C)
   (newtonmaterialsetcontactfrictionstate
    (%material-pair-ptr material-pair)
    state
    index)
-  (values))
+  nil)
 
-(defun material-pair-set-contact-softness (material-pair softness)
+(defn material-pair-set-contact-softness ((material-pair material-pair)
+                                          (softness single-float))
+    null
   (declare (optimize (speed 3) (safety 1) (debug 1)))
   (newtonmaterialsetcontactsoftness
    (%material-pair-ptr material-pair)
    softness)
-  (values))
+  nil)
 
-(defun material-pair-set-contact-tangent-acceleration (material-pair
-                                                       acceleration
-                                                       index)
+(defn material-pair-set-contact-tangent-acceleration ((material-pair material-pair)
+                                                      (acceleration single-float)
+                                                      (index (signed-byte 32)))
+    null
   (declare (optimize (speed 3) (safety 1) (debug 1)))
   (newtonmaterialsetcontacttangentacceleration
    (%material-pair-ptr material-pair)
    acceleration
    index)
-  (values))
+  nil)
 
-(defun material-pair-set-contact-tangent-friction (material-pair
-                                                   friction
-                                                   index)
+(defn material-pair-set-contact-tangent-friction ((material-pair material-pair)
+                                                  (friction single-float)
+                                                  (index (signed-byte 32)))
+    null
   (declare (optimize (speed 3) (safety 1) (debug 1)))
   (newtonmaterialsetcontacttangentfriction
    (%material-pair-ptr material-pair)
    friction
    index)
-  (values))
+  nil)
 
-(defun world-set-default-material-collidable (world
-                                              material-a
-                                              material-b
-                                              state)
+(defn world-set-default-material-collidable ((world world)
+                                             (material-a (signed-byte 32))
+                                             (material-b (signed-byte 32))
+                                             (state (signed-byte 32)))
+    null
   (declare (optimize (speed 3) (safety 1) (debug 1)))
   ;; {TODO} how do we represent state?
   (newtonmaterialsetdefaultcollidable
@@ -186,25 +214,28 @@
    material-a
    material-b
    state)
-  (values))
+  nil)
 
-(defun world-set-default-material-elasticity (world
-                                              material-a
-                                              material-b
-                                              elasticity)
+(defn world-set-default-material-elasticity (world
+                                             (material-a (signed-byte 32))
+                                             (material-b (signed-byte 32))
+                                             (elasticity single-float))
+    null
   (declare (optimize (speed 3) (safety 1) (debug 1)))
   (newtonmaterialsetdefaultelasticity
    (%world-ptr world)
    material-a
    material-b
    elasticity)
-  (values))
+  nil)
 
-(defun world-set-default-material-friction (world
-                                            material-a
-                                            material-b
-                                            static-friction-coefficient
-                                            kinetic-friction-coefficient)
+(defn world-set-default-material-friction
+    ((world world)
+     (material-a (signed-byte 32))
+     (material-b (signed-byte 32))
+     (static-friction-coefficient single-float)
+     (kinetic-friction-coefficient single-float))
+    null
   (declare (optimize (speed 3) (safety 1) (debug 1)))
   (newtonmaterialsetdefaultfriction
    (%world-ptr world)
@@ -212,40 +243,50 @@
    material-b
    static-friction-coefficient
    kinetic-friction-coefficient)
-  (values))
+  nil)
 
-(defun world-set-default-material-softness (world
-                                            material-a
-                                            material-b
-                                            softness)
+(defn world-set-default-material-softness ((world world)
+                                           (material-a (signed-byte 32))
+                                           (material-b (signed-byte 32))
+                                           (softness single-float))
+    null
   (declare (optimize (speed 3) (safety 1) (debug 1)))
   (newtonmaterialsetdefaultsoftness
    (%world-ptr world)
    material-a
    material-b
    softness)
-  (values))
+  nil)
 
-(defun world-set-material-pair-surface-thickness (world
-                                                  material-a
-                                                  material-b
-                                                  thickness)
+(defn world-set-material-pair-surface-thickness ((world world)
+                                                 (material-a (signed-byte 32))
+                                                 (material-b (signed-byte 32))
+                                                 (thickness single-float))
+    null
   (declare (optimize (speed 3) (safety 1) (debug 1)))
   (newtonmaterialsetsurfacethickness
    (%world-ptr world)
    material-a
    material-b
    thickness)
-  (values))
+  nil)
 
-(defun material-contact-rotate-tangent-directions (material-pair vec3)
+(defn material-contact-rotate-tangent-directions
+    ((material-pair material-pair)
+     (vec3 (simple-array single-float (3))))
+    null
   (declare (optimize (speed 3) (safety 1) (debug 1)))
   (with-foreign-array (v3 vec3 '(:array :float 3))
     (newtonmaterialcontactrotatetangentdirections
      (%material-pair-ptr material-pair)
-     v3)))
+     v3))
+  nil)
 
-(defun material-pair-contact-position-and-normal (material-pair body)
+(defn material-pair-contact-position-and-normal
+    ((material-pair material-pair)
+     (body body))
+    (values (simple-array single-float (3))
+            (simple-array single-float (3)))
   (declare (optimize (speed 3) (safety 1) (debug 1)))
   (with-foreign-objects ((pos3 :float 3)
                          (norm3 :float 3))
@@ -254,7 +295,9 @@
      (%body-ptr body) pos3 norm3)
     (values (ptr->v3 pos3) (ptr->v3 norm3))))
 
-(defun material-pair-contact-position (material-pair body)
+(defn material-pair-contact-position ((material-pair material-pair)
+                                      (body body))
+    (simple-array single-float (3))
   (declare (optimize (speed 3) (safety 1) (debug 1)))
   (with-foreign-objects ((pos3 :float 3)
                          (norm3 :float 3))
@@ -263,7 +306,9 @@
      (%body-ptr body) pos3 norm3)
     (ptr->v3 pos3)))
 
-(defun material-pair-contact-normal (material-pair body)
+(defn material-pair-contact-normal ((material-pair material-pair)
+                                    (body body))
+    (simple-array single-float (3))
   (declare (optimize (speed 3) (safety 1) (debug 1)))
   (with-foreign-objects ((pos3 :float 3)
                          (norm3 :float 3))
@@ -272,7 +317,10 @@
      (%body-ptr body) pos3 norm3)
     (ptr->v3 norm3)))
 
-(defun (setf material-pair-contact-position) (pos3 material-pair)
+(defn (setf material-pair-contact-position)
+    ((pos3 (simple-array single-float (3)))
+     (material-pair material-pair))
+    (simple-array single-float (3))
   (declare (optimize (speed 3) (safety 1) (debug 1)))
   (with-foreign-array (p3 pos3 '(:array :float 3))
     (newtonmaterialsetcontactposition
@@ -280,7 +328,10 @@
      p3))
   pos3)
 
-(defun (setf material-pair-contact-normal) (vec3 material-pair)
+(defn (setf material-pair-contact-normal)
+    ((vec3 (simple-array single-float (3)))
+     (material-pair material-pair))
+    (simple-array single-float (3))
   (declare (optimize (speed 3) (safety 1) (debug 1)))
   (with-foreign-array (n3 vec3 '(:array :float 3))
     (newtonmaterialsetcontactnormaldirection
